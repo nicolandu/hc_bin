e01bfb14e480807eefcf1e8cc7c16fff071b3bba
Nicolas Landucci <74741743+nicolandu@users.noreply.github.com>
2024-07-30 22:25:18 -0400

    Start decode logic

--- sketch.ino
+++ sketch.ino
@@ -1,11 +1,35 @@
+#include <algorithm>
+#include <string>
+#include <list>
+
+const std::array<std::string, 4> letters = {".-", "-...", "-.-.", "-.."};
+
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
   pinMode(27, INPUT_PULLUP);
+  while (digitalRead(27) != LOW) {};
 }
 
 void loop() {
-  if (digitalRead(27) == LOW) {
-    Serial.println("a");
+  std::string seq = "";
+  for (bool in_word = true; in_word;) {
+
+    unsigned long long delta = millis();
+    while (digitalRead(27) != HIGH) {};
+    delta = millis() - delta;
+
+    seq += (delta > 300) ? '-' : '.';
+    Serial.print(delta);
+
+    delta = millis();
+    while (digitalRead(27) != LOW) {};
+    delta = millis() - delta;
+    Serial.println(delta);
+
+    if (delta > 300) in_word = false;
   }
-  Serial.println("b");
+  auto it = std::find(letters.begin(), letters.end(), seq);
+  if (it != letters.end()) {
+    Serial.print('A'+(it-letters.begin()));
+  }
 }
